78a75d648df60429dd6c693b4bdeb9173293412c
咚咚多吃菜 <2840219564@qq.com>
2023-01-03 20:32:00 +0800

    All_WuHu
    五虎功能汇总

---
+++ All_WuHu/自动挂机/ObjManager.cs
@@ -0,0 +1,21 @@
+using Games.LogicObj;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjManager : MonoBehaviour
+{
+    private Obj_MainPlayer m_MainPlayer;                //当前客户端主角色
+    public Obj_MainPlayer MainPlayer { get { return m_MainPlayer; } }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
